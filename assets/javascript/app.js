d0c3167ff786316a49dc3e0d7374d10e20807f9b
chrisxjacobi <chrisxjacobi@gmail.com>
2016-08-22 23:14:04 -0500

    questions and pics

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,26 +7,75 @@
 -questions vs correctAnswer = win; correctAnswer < questions = loss
 -setTimer for page
 
+-create questionArray with all Questions
+-create answerArray with all Answers
+-create correctAnswerArray with the correct answers
+
 
 */
 
 
-var questions = [{
-        question: "Who is the frontman for Emperor?",
-        answer: ["Ihsahn", "Necrobutcher", "Gaahl", "Matt Pike"],
-        correctAnswer: 0,
-    },
+var questionOne = {
+    question: "Who is the frontman for Emperor?",
+    answer: ["Ihsahn", "Necrobutcher", "Gaahl", "Nocturno Culto"],
+    correctAnswer: 0,
+};
 
-    {
-        question: "Who released the classic album 'Filosofem'?",
-        answer: ["Mayhem", "Dark Fortress", "Burzum", "Venom"],
-        correctAnswer: 2,
-    },
+var questionTwo = {
+    question: "What band released the classic album 'Filosofem'?",
+    answer: ["Mayhem", "Dark Fortress", "Burzum", "Venom"],
+    correctAnswer: 2,
+};
 
-    {
-    	question: "Celtic Frost rose from the ashes of what band?"
-    },
-]
+var questionThree = {
+    question: "Celtic Frost rose from the ashes of what band?",
+    answer: ["Mortuary Drape", "Hellhammer", "Goblin", "Darkthrone"],
+    correctAnswer: 1,
+};
+
+var questionFour = {
+    question: "What band released the 2013 genre-defying 'Sunbather'?",
+    answer: ["Woe", "Marduk", "Deafheaven", "Enslaved"],
+    correctAnswer: 2,
+};
+
+var questionFive = {
+    question: "Complete the band name: _____ In The Throne Room",
+    answer: ["Drapes", "Goats", "Wolves", "Dreams"],
+    correctAnswer: 2,
+};
+
+var questionSix = {
+    question: "Which of the following is NOT an album by Watain?",
+    answer: ["Lawless Darkness", "Evangelion", "The Wild Hunt", "Sworn to the Dark"],
+    correctAnswer: 1,
+};
+
+var questionSeven = {
+	question: "Which French band has managed to keep their personal identities private for over almost 20 years?",
+	answer: ["Cilice", "Gojira", "Celeste", "Deathspell Omega"],
+	correctAnswer: 3,
+};
+
+var questionEight = {
+	question: "Which band coined the term 'Black Metal' with the title of their 1982 classic album?",
+	answer: ["Venom", "Ascension", "Dispirit", "Weakling"],
+	correctAnswer: 0,
+};
+
+var questionNine = {
+	question: "Legendary act Bathory was named after noblewoman Countess Elisabeth Bathory of which Eastern European country?",
+	answer: ["Moldova", "Poland", "Slovakia", "Hungary"],
+	correctAnswer: 3,
+};
+
+var questionTen = {
+	question: "Immortal has based their lyrical content on the events of what fictitious world?",
+	answer: ["Isa", "Blashyrkh", "Von", "Divinus"],
+	correctAnswer: 1,
+}
+
+
 
 $(document).ready(function() {
 
@@ -34,7 +83,8 @@ $(document).ready(function() {
         $(".startWindow").addClass("invisible");
         $(".questionArea").removeClass("invisible");
         //start countdown timer
-        displayQuestion();
+        $('.questionArea').html(questionThree.question);
+        //build content of form in jQuery (input type = "radio"...)
     });
 
 
@@ -43,8 +93,8 @@ $(document).ready(function() {
 
 
     function displayQuestion() {
-    	for (i = 0; i < questions.length; i++) {
-    		console.log(questions[i].question);
-    	}
+        for (i = 0; i < question.length; i++) {
+            console.log(questions[i].question);
+        }
     }
 });
