b5aab8fb64e277aba758ace59e15607955ea899c
chrisxjacobi <chrisxjacobi@gmail.com>
2016-08-26 14:03:04 -0500

    fixed submit button placement

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -32,61 +32,61 @@ var game = {
 
 
 var questionOne = {
-    question: "Who is the frontman for Emperor?",
+    question: "1. Who is the frontman for Emperor?",
     answer: ["Ihsahn", "Necrobutcher", "Gaahl", "Nocturno Culto"],
     correctAnswer: 0,
 };
 
 var questionTwo = {
-    question: "What band released the classic album 'Filosofem'?",
+    question: "2. What band released the classic album 'Filosofem'?",
     answer: ["Mayhem", "Dark Fortress", "Burzum", "Venom"],
     correctAnswer: 2,
 };
 
 var questionThree = {
-    question: "Celtic Frost rose from the ashes of what band?",
+    question: "3. Celtic Frost rose from the ashes of what band?",
     answer: ["Mortuary Drape", "Hellhammer", "Goblin", "Darkthrone"],
     correctAnswer: 1,
 };
 
 var questionFour = {
-    question: "What band released the 2013 genre-defying 'Sunbather'?",
+    question: "4. What band released the 2013 genre-defying 'Sunbather'?",
     answer: ["Woe", "Marduk", "Deafheaven", "Enslaved"],
     correctAnswer: 2,
 };
 
 var questionFive = {
-    question: "Complete the band name: _____ In The Throne Room",
+    question: "5. Complete the band name: _____ In The Throne Room",
     answer: ["Drapes", "Goats", "Wolves", "Dreams"],
     correctAnswer: 2,
 };
 
 var questionSix = {
-    question: "Which of the following is NOT an album by Watain?",
+    question: "6. Which of the following is NOT an album by Watain?",
     answer: ["Lawless Darkness", "Evangelion", "The Wild Hunt", "Sworn to the Dark"],
     correctAnswer: 1,
 };
 
 var questionSeven = {
-    question: "Which French band has managed to keep their personal identities private for over almost 20 years?",
+    question: "7. Which French band has managed to keep their personal identities private for almost 20 years?",
     answer: ["Cilice", "Gojira", "Celeste", "Deathspell Omega"],
     correctAnswer: 3,
 };
 
 var questionEight = {
-    question: "Which band coined the term 'Black Metal' with the title of their 1982 classic album?",
+    question: "8. Which band coined the term 'Black Metal' with the title of their 1982 classic album?",
     answer: ["Venom", "Ascension", "Dispirit", "Weakling"],
     correctAnswer: 0,
 };
 
 var questionNine = {
-    question: "Legendary act Bathory was named after noblewoman Countess Elisabeth Bathory of which Eastern European country?",
+    question: "9. Legendary act Bathory was named after noblewoman Countess Elisabeth Bathory of which Eastern European country?",
     answer: ["Moldova", "Poland", "Slovakia", "Hungary"],
     correctAnswer: 3,
 };
 
 var questionTen = {
-    question: "Immortal has based their lyrical content on the events of what fictitious world?",
+    question: "10. Immortal has based their lyrical content on the events of what fictitious world?",
     answer: ["Isa", "Blashyrkh", "Von", "Divinus"],
     correctAnswer: 1,
 }
@@ -109,20 +109,25 @@ $(document).ready(function() {
     $('.startButton').on('click', function() {
         $('.startWindow').addClass('invisible');
         $('.questionArea').removeClass('invisible');
+
+        var submitButton = $('<button>');
+            submitButton.addClass('submit');
+            submitButton.text('Submit');
+            $('.wrapper').append(submitButton);
+            
         game.start();
         timer = setInterval(game.clock, 1000)
         $('.questionArea').prepend("Time remaining: <span id='counter'>90</span> Seconds");
         for (var i = 0; i < questions.length; i++) {
-            // <div class="question">
-            //   <div class="question-text"></div>
-            //   <div class="options"></div>
             var showQuestion = $('<div class="question">');
             var questionText = $('<div class="question-text">');
             questionText.html(questions[i].question);
             showQuestion.append(questionText);
 
+
             var answerGroup = $('<form class="answers">');
             showQuestion.append(answerGroup);
+
 
             for (var j = 0; j < questions[i].answer.length; j++) {
                 var showAnswer = $('<div class="answer"><input type="radio' + '" name="radio">' + questions[i].answer[j] + '</div>');
@@ -130,19 +135,29 @@ $(document).ready(function() {
             }
 
             $('.questionArea').append(showQuestion);
-        }
-        $('questionArea').append('<button</> class="submit"')
 
-      /*  result: function() {
-            clearInterval(timer);
-            $('.questionArea').remove();
-            $('.questionArea').html("Finished!")
-            $('.questionArea').append("Correct Answers: " + this.correct);
-            $('.questionArea').append("Incorrect Answers: " + this.incorrect);
-        }; */
+
+
+        }
+
+
+        /*  result: function() {
+              clearInterval(timer);
+              $('.questionArea').remove();
+              $('.questionArea').html("Finished!")
+              $('.questionArea').append("Correct Answers: " + this.correct);
+              $('.questionArea').append("Incorrect Answers: " + this.incorrect);
+          }; */
 
     });
 
+
+
+
+
+    // <div class="question">
+    //   <div class="question-text"></div>
+    //   <div class="options"></div>
 
 
 
